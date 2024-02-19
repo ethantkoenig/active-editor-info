2ef6ec73d8afbca853770e23c8c483774cb9d02a
Ethan Koenig <ethantkoenig@gmail.com>
2024-02-19 13:15:23 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# active-editor-info
+
+VSCode extension that provides commands returning various info about the active editor.
+
+The motivating (and for now only) use case is to expose info about the active editor to Talon.
+
+## Commands
+
+- `activeEditorInfo.languageId`: returns the language id of the active editor
---
+++ src/extension.ts
@@ -0,0 +1,12 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+
+	let disposable = vscode.commands.registerCommand('activeEditorInfo.languageId', () => {
+		return vscode.window.activeTextEditor?.document.languageId;
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() { }
