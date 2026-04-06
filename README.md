a1751e5798d75dc45fa1394108ea391c6dc08ae9
Ethan Koenig <ethantkoenig@gmail.com>
2026-04-05 17:34:54 -0700

    `activeEditorInfo.get` command

--- README.md
+++ README.md
@@ -6,4 +6,37 @@ The motivating (and for now only) use case is to expose info about the active ed
 
 ## Commands
 
-- `activeEditorInfo.languageId`: returns the language id of the active editor, or `null` if there is no active editor
+### `activeEditorInfo.get`
+
+Returns a JSON object with various info about the active editor (or `null` if
+there is no active editor). The JSON object will be of the form:
+
+```jsonc
+{
+    // Absolute path of the file.
+    "absoluteFilePath": "...",
+    // VSCode language ID of the file.
+    "languageId": "...",
+    // Path, relative to the root of the opened workspace. May be `null` if there is no workspace.
+    "relativeFilePath": "...",
+    // Array of selections in the active editor. The first selection is the "primary" one.
+    //
+    // Modeled after https://code.visualstudio.com/api/references/vscode-api#Selection.
+    "selections": [
+        {
+            "anchor": { "line": N, "column": M },  // zero-indexed
+            "cursor": { "line": N, "column": M },  // zero-indexed
+            "isReversed": false
+        }
+    ],
+    // Array of visible ranges in the active editor.
+    //
+    // Modeled after https://code.visualstudio.com/api/references/vscode-api#Range.
+    "visibleRanges": [
+        {
+            "start": { "line": N, "column": M },  // zero-indexed
+            "end": { "line": N, "column": M }     // zero-indexed
+        }
+    ]
+}
+```
--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,42 @@
+import { relative } from "path";
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand(
-    "activeEditorInfo.languageId",
-    () => {
-      return vscode.window.activeTextEditor?.document.languageId || null;
-    }
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "activeEditorInfo.get",
+      () => {
+        const activeTextEditor = vscode.window.activeTextEditor;
+        if (!activeTextEditor) {
+          return null;
+        }
+        const workspaceFolder = vscode.workspace.getWorkspaceFolder(activeTextEditor.document.uri);
+        return {
+          absoluteFilePath: activeTextEditor.document.fileName,
+          languageId: activeTextEditor.document.languageId,
+          relativeFilePath: workspaceFolder
+            ? relative(workspaceFolder.uri.fsPath, activeTextEditor.document.fileName)
+            : null,
+          selections: activeTextEditor.selections.map(selectionToObject),
+          visibleRanges: activeTextEditor.visibleRanges.map((range) => ({
+            start: positionToObject(range.start), end: positionToObject(range.end)
+          }))
+        };
+      }
+    )
   );
-
-  context.subscriptions.push(disposable);
 }
 
-export function deactivate() {}
+function selectionToObject(selection: vscode.Selection) {
+  return {
+    anchor: positionToObject(selection.anchor),
+    cursor: positionToObject(selection.active),
+    isReversed: selection.isReversed
+  };
+}
+
+function positionToObject(position: vscode.Position) {
+  return { column: position.character, line: position.line };
+}
+
+export function deactivate() { }
